d786d901a3d422a059b7b982e21d89367e3a982d
Paradize02 <andrejsevcov946@gmail.com>
2024-09-06 12:05:01 +0300

    fixed

--- src/components/payment/Ready.jsx
+++ src/components/payment/Ready.jsx
@@ -3,8 +3,13 @@ import './Pay.css';
 
 const Ready = () => {
    
+    const handleClick = () => {
+        window.location.href = '/main';
+      };
+
+
     return (
-        <div className="Ready">
+        <div className="Ready" onClick={handleClick}>
             <div className='Ready_img'></div>
             <div className='ReadyText1'>Напиток готов!</div>
             <div className='ReadyText2'>вы можете забрать его</div>
